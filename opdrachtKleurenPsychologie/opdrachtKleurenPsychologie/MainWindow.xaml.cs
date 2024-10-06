873e8ce615b0d3ee681e915ad9e05f5115152fff
quintendelvaux <12400116@student.pxl.be>
2024-10-06 12:46:37 +0200

    KLEUR-03

--- opdrachtKleurenPsychologie/opdrachtKleurenPsychologie/MainWindow.xaml.cs
+++ opdrachtKleurenPsychologie/opdrachtKleurenPsychologie/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,5 +21,26 @@ namespace opdrachtKleurenPsychologie
         {
             InitializeComponent();
         }
+
+        private void redImage_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void yellowImage_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Yellow);
+        }
+
+        private void greenImage_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Green);
+        }
+
+        private void blueImage_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Blue);
+        }
     }
+    
 }
